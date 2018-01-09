dc338ec5402fe94e940862113f3043d42bef8d0c
JPCOLONNA <s-nascimento@jpcolonna.fr>
2018-01-09 17:21:26 +0100

    Commit beneficiaires

--- src/app/base-tampon/components/formulaire-assure/formulaire-assure.component.ts
+++ src/app/base-tampon/components/formulaire-assure/formulaire-assure.component.ts
@@ -17,11 +17,16 @@ export class FormulaireAssureComponent implements OnInit {
   /** Ressources globales : label commun à toutes les applications<br /> Fichier : resources/_resources.json */
   rsc: any;
 
+  /**Le formulaire concerne soit un assuré soit un bénéficaire   */
+
   /**Données de l'assuré en cours d'affichage/de modification*/
   @Input() assure: any;
 
+  /**Données du bénéficiaire en cours d'affichage/de modification*/
+  @Input() beneficiaire: any;
+
   /** Action effectuée sur l'écran : consulter ou modifier */
-  @Input() action:string;
+  @Input() action: string;
 
   // --------------------- Formulaire -----------------------
   /** Nom du formulaire de recherche */
@@ -51,10 +56,10 @@ export class FormulaireAssureComponent implements OnInit {
   ngOnInit() {
 
     // Attribut la valeur par défaut
-    if(this.action == "")
+    if (this.action == "")
       this.action = "consulter";
-    
-    if(this.action == "modifier")
+
+    if (this.action == "modifier")
       this.disabled = false;
 
     //Ressources générales
@@ -68,32 +73,66 @@ export class FormulaireAssureComponent implements OnInit {
    * Création du formulaire d'un assuré dans la base tampon
    */
   createForm(): void {
-    this.formAssure = this.formBuilder.group({
-      'civilite': [{value:this.assure.civilite, disabled:this.disabled}],
-      'nom': [{value:this.assure.nom, disabled:this.disabled}],
-      'prenom': [{value:this.assure.prenom, disabled:this.disabled}],
-      'adresse': [{value:this.assure.adresse, disabled:this.disabled}],
-      'adresse_suite': [{value:this.assure.adresse_suite, disabled:this.disabled}],
-      'code_postal': [{value:this.assure.code_postal, disabled:this.disabled}],
-      'ville': [{value:this.assure.ville, disabled:this.disabled}],
-      'telephone': [{value:this.assure.telephone, disabled:this.disabled}],
-      'fax': [{value:this.assure.fax, disabled:this.disabled}],
-      'email': [{value:this.assure.email, disabled:this.disabled}],
-      'code_banque': [{value:this.assure.code_banque, disabled:this.disabled}],
-      'guichet_banque': [{value:this.assure.guichet_banque, disabled:this.disabled}],
-      'num_compte': [{value:this.assure.num_compte, disabled:this.disabled}],
-      'cle_compte': [{value:this.assure.cle_compte, disabled:this.disabled}],
-      'date_naissance': [{value:this.assure.date_naissance, disabled:this.disabled}],
-      'situation_familiale': [{value:this.assure.situation_familiale, disabled:this.disabled}],
-      'nombre_enfant': [{value:this.assure.nombre_enfant, disabled:this.disabled}],
-      'transfert_noemie': [{value:this.assure.transfert_noemie, disabled:this.disabled}],
-      'regime': [{value:this.assure.regime, disabled:this.disabled}],
-      'caisse': [{value:this.assure.caisse, disabled:this.disabled}],
-      'guichet': [{value:this.assure.guichet, disabled:this.disabled}],
-      'cle': [{value:this.assure.cle, disabled:this.disabled}],
-      'num_ss': [{value:this.assure.num_ss, disabled:this.disabled}],
-      'cle_ss': [{value:this.assure.cle_ss, disabled:this.disabled}]
-    });
+    if (this.assure !== undefined) {
+      this.formAssure = this.formBuilder.group({
+        'civilite': [{ value: this.assure.civilite, disabled: this.disabled }],
+        'nom': [{ value: this.assure.nom, disabled: this.disabled }],
+        'prenom': [{ value: this.assure.prenom, disabled: this.disabled }],
+        'adresse': [{ value: this.assure.adresse, disabled: this.disabled }],
+        'adresse_suite': [{ value: this.assure.adresse_suite, disabled: this.disabled }],
+        'code_postal': [{ value: this.assure.code_postal, disabled: this.disabled }],
+        'ville': [{ value: this.assure.ville, disabled: this.disabled }],
+        'telephone': [{ value: this.assure.telephone, disabled: this.disabled }],
+        'fax': [{ value: this.assure.fax, disabled: this.disabled }],
+        'email': [{ value: this.assure.email, disabled: this.disabled }],
+        'code_banque': [{ value: this.assure.code_banque, disabled: this.disabled }],
+        'guichet_banque': [{ value: this.assure.guichet_banque, disabled: this.disabled }],
+        'num_compte': [{ value: this.assure.num_compte, disabled: this.disabled }],
+        'cle_compte': [{ value: this.assure.cle_compte, disabled: this.disabled }],
+        'date_naissance': [{ value: this.assure.date_naissance, disabled: this.disabled }],
+        'situation_familiale': [{ value: this.assure.situation_familiale, disabled: this.disabled }],
+        'nombre_enfant': [{ value: this.assure.nombre_enfant, disabled: this.disabled }],
+        'transfert_noemie': [{ value: this.assure.transfert_noemie, disabled: this.disabled }],
+        'regime': [{ value: this.assure.regime, disabled: this.disabled }],
+        'caisse': [{ value: this.assure.caisse, disabled: this.disabled }],
+        'guichet': [{ value: this.assure.guichet, disabled: this.disabled }],
+        'cle': [{ value: this.assure.cle, disabled: this.disabled }],
+        'num_ss': [{ value: this.assure.num_ss, disabled: this.disabled }],
+        'cle_ss': [{ value: this.assure.cle_ss, disabled: this.disabled }]
+      });
+    } else {
+            this.formAssure = this.formBuilder.group({
+        'civilite': [{ value: this.beneficiaire.civilite, disabled: this.disabled }],
+        'nom': [{ value: this.beneficiaire.nom, disabled: this.disabled }],
+        'prenom': [{ value: this.beneficiaire.prenom, disabled: this.disabled }],
+        'adresse': [{ value: this.beneficiaire.adresse, disabled: this.disabled }],
+        'adresse_suite': [{ value: this.beneficiaire.adresse_suite, disabled: this.disabled }],
+        'code_postal': [{ value: this.beneficiaire.code_postal, disabled: this.disabled }],
+        'ville': [{ value: this.beneficiaire.ville, disabled: this.disabled }],
+        'telephone': [{ value: this.beneficiaire.telephone, disabled: this.disabled }],
+        'fax': [{ value: this.beneficiaire.fax, disabled: this.disabled }],
+        'email': [{ value: this.beneficiaire.email, disabled: this.disabled }],
+        'code_banque': [{ value: this.beneficiaire.code_banque, disabled: this.disabled }],
+        'guichet_banque': [{ value: this.beneficiaire.guichet_banque, disabled: this.disabled }],
+        'num_compte': [{ value: this.beneficiaire.num_compte, disabled: this.disabled }],
+        'cle_compte': [{ value: this.beneficiaire.cle_compte, disabled: this.disabled }],
+        'date_naissance': [{ value: this.beneficiaire.date_naissance, disabled: this.disabled }],
+        'situation_familiale': [{ value: this.beneficiaire.situation_familiale, disabled: this.disabled }],
+        'nombre_enfant': [{ value: this.beneficiaire.nombre_enfant, disabled: this.disabled }],
+        'transfert_noemie': [{ value: this.beneficiaire.transfert_noemie, disabled: this.disabled }],
+        'regime': [{ value: this.beneficiaire.regime, disabled: this.disabled }],
+        'caisse': [{ value: this.beneficiaire.caisse, disabled: this.disabled }],
+        'guichet': [{ value: this.beneficiaire.guichet, disabled: this.disabled }],
+        'cle': [{ value: this.beneficiaire.cle, disabled: this.disabled }],
+        'num_ss': [{ value: this.beneficiaire.num_ss, disabled: this.disabled }],
+        'cle_ss': [{ value: this.beneficiaire.cle_ss, disabled: this.disabled }],
+        'qualite': [{ value: this.beneficiaire.qualite, disabled: this.disabled }],
+        'rang': [{ value: this.beneficiaire.rang, disabled: this.disabled }],
+        'attachement': [{ value: this.beneficiaire.attachement, disabled: this.disabled }],
+        'debut_effet': [{ value: new Date(this.beneficiaire.debut_effet), disabled: this.disabled }],
+        'fin_effet': [{ value: new Date(this.beneficiaire.fin_effet), disabled: this.disabled }]
+      });
+    }
   }
 
 }
